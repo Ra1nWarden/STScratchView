8a27a1b0b2a0347ad195d88be01ecf3411f4e315
Ra1nWarden <wzh19921016@gmail.com>
2015-02-23 13:28:23 -0500

    Added tracking protocol

--- STScratchView/STScratchView.h
+++ STScratchView/STScratchView.h
@@ -6,10 +6,19 @@
 //  Copyright (c) 2012 Sebastien Thiebaud. All rights reserved.
 //
 
+@class STScratchView;
+
+@protocol STScratchViewDelegate
+
+- (void)STScratchView:(STScratchView*)scratchView didChangeProgress:(CGFloat)progress;
+
+@end
+
 @interface STScratchView : UIView
 
 @property (nonatomic, assign) float sizeBrush;
 @property (nonatomic, strong) UIView *hideView;
+@property (nonatomic, weak) id<STScratchViewDelegate> delegate;
 
 - (void)setHideView:(UIView *)hideView;
 - (void)setAutomaticScratchCurve:(UIBezierPath *)curvePath duration:(float)duration;
